635e9765e23e673b7ca304c631c8df9a482143a1
vi2hnu <vishnuvaradhankr@gmail.com>
2025-12-14 12:30:35 +0530

    Added: error handeling for login page

--- src/app/component/login/login.ts
+++ src/app/component/login/login.ts
@@ -1,18 +1,22 @@
 import { Component, inject } from '@angular/core';
 import { LoginModel } from '../../model/login/login-model.model';
 import { FormsModule } from '@angular/forms';
-import { LoginService } from '../../service/login/login-service';
+import { LoginService } from '../../service/login/login-service';       
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
   loginModel: LoginModel = new LoginModel();
   loginService = inject(LoginService);
-  
+  loginError: boolean = false;
+  errorMessage: string = '';
+
+
   login() {
     this.loginService.login(this.loginModel).subscribe(
       (response) => {
@@ -20,8 +24,19 @@ export class Login {
         alert('Login successful');
       },
       (error) => {
+        this.loginError = true;
+        if(error.status === 401){
+          this.errorMessage = 'Username or password is wrong';
+        }
+        else  {
+          this.errorMessage = 'Internal Server Error. Please try again later.';
+        }
         console.error('Login failed', error);
       }
     );
+  }
+
+  hidePopUp() {
+    this.loginError = false;
   }
 }
